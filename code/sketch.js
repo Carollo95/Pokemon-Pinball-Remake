c27408eb5845507da134d0d70241d398c14dce2b
Pedro <carollo95@gmail.com>
2025-07-30 11:57:34 +0200

    Flippers on the right position

--- code/sketch.js
+++ code/sketch.js
@@ -1,22 +1,19 @@
 let ball;
 let leftFlipper, rightFlipper;
 
-let BALL_DIAMETER = 32;
+let bg;
 
-let FLIPPER_LENGTH = 48;
-let FLIPPER_WIDTH = 18
-
-let LEFT_FLIPPER_ROTATION_POINT_X = 114;
-let LEFT_FLIPPER_ROTATION_POINT_Y = 516;
-
+  
 
 function setup() {
-  createCanvas(320, 576);
+  createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  bg = loadImage('assets/img/background_test.png');
 
   createBall();
   createFlippers()
 
-  world.gravity.y = 10;
+  world.gravity.y = GRAVITY;
 
   new Sprite(400, 580, 800, 40, 'static'); // Remove just a test
 }
@@ -26,15 +23,20 @@ function createBall() {
   ball.x = 150;
   ball.y = 250;
   ball.diameter = BALL_DIAMETER;
+  ball.debug = DEBUG;
 }
 
 function createFlippers(){
 
-  leftFlipper = new Sprite(114, 516, FLIPPER_LENGTH, FLIPPER_WIDTH, 'kinematic');
-  leftFlipper.rotation = 30;
+  leftFlipper = new Sprite(LEFT_FLIPPER_ROTATION_POINT_X, LEFT_FLIPPER_ROTATION_POINT_Y, FLIPPER_LENGTH, FLIPPER_WIDTH, 'kinematic');
+  leftFlipper.rotation = 35;
+  leftFlipper.debug = DEBUG;
+  leftFlipper.offset.x = 14;
 
-  rightFlipper = new Sprite(174, 516, FLIPPER_LENGTH, FLIPPER_WIDTH, 'kinematic');
-  rightFlipper.rotation = 30;
+  rightFlipper = new Sprite(RIGHT_FLIPPER_ROTATION_POINT_X, RIGHT_FLIPPER_ROTATION_POINT_Y, FLIPPER_LENGTH, FLIPPER_WIDTH, 'kinematic');
+  rightFlipper.rotation = -35
+  rightFlipper.debug = DEBUG;
+  rightFlipper.offset.x = -14;
 
   // To prevent the flipper of doing full 360s
   leftFlipper.body.setFixedRotation(true);
@@ -43,19 +45,19 @@ function createFlippers(){
 }
 
 function draw() {
-  background(200);
+  background(bg);
 
-
-  if (kb.pressing('a')) {
-    if (rightFlipper.rotation < 30) rightFlipper.rotation += 30;
+ 
+  if (kb.pressing('l')) {
+    if (rightFlipper.rotation < 15) rightFlipper.rotation += 15;
   } else {
-    if (rightFlipper.rotation > -30) rightFlipper.rotation -= 30;
+    if (rightFlipper.rotation > -35) rightFlipper.rotation -= 15;
   }
 
 
-  if (kb.pressing('l')) {
-    if (leftFlipper.rotation > -30) leftFlipper.rotation -= 30;
+  if (kb.pressing('a')) {
+    if (leftFlipper.rotation > -15) leftFlipper.rotation -= 15;
   } else {
-    if (leftFlipper.rotation < 30) leftFlipper.rotation += 30;
+    if (leftFlipper.rotation < 35) leftFlipper.rotation += 15;
   }
 }
