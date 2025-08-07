cdd1b10087b6966ed562e1e0afb45b737ba2e327
Pedro <carollo95@gmail.com>
2025-08-07 16:52:31 +0200

    Show fps on debug

--- code/sketch.js
+++ code/sketch.js
@@ -1,5 +1,6 @@
 let stage;
 
+
 function setup() {
   stage = new BonusStageGhost();
   stage.setup();
@@ -7,6 +8,16 @@ function setup() {
 
 
 function draw() {
-stage.draw();
+  stage.draw();
+  if (DEBUG) {
+    showFPS();
+  }
+}
+
+function showFPS() {
+  let fps = frameRate();
+  fill(255);
+  stroke(0);
+  text("FPS: " + fps.toFixed(2), 10, 10);
 }
 
