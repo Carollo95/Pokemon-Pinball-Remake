a295ae9fe934edf7616c430e444e78ebc0e91c28
Pedro <carollo95@gmail.com>
2025-08-07 19:24:06 +0200

    reduced flipper thickness

--- code/flippers.js
+++ code/flippers.js
@@ -1,5 +1,5 @@
 const FLIPPER_LENGTH = 48; //Length of the flipper
-const FLIPPER_WIDTH = 18 //Height of the flipper
+const FLIPPER_WIDTH = 2; //Height of the flipper
 
 const LEFT_FLIPPER_ROTATION_POINT_X = 111; //Horizontal pixel for the left flipper axis of rotation
 const LEFT_FLIPPER_ROTATION_POINT_Y = 515; //Vertical pixel for the left flipper axis of rotation
@@ -15,7 +15,7 @@ const BONUS_RIGHT_FLIPPER_ROTATION_POINT_Y = BONUS_LEFT_FLIPPER_ROTATION_POINT_Y
 
 const LEFT_FLIPPER_MIN_ROTATION = 35; //Min angle for the left flipper
 const RIGHT_FLIPPER_MIN_ROTATION = LEFT_FLIPPER_MIN_ROTATION * -1; //Min angle for the right flipper
-const LEFT_FLIPPER_MAX_ROTATION = -19; //Max angle for the left flipper
+const LEFT_FLIPPER_MAX_ROTATION = -35; //Max angle for the left flipper
 const RIGHT_FLIPPER_MAX_ROTATION = LEFT_FLIPPER_MAX_ROTATION * -1; //Max angle for the right flipper
 
 const RIGHT_FLIPPER_ROTATION_SPEED = 15; //Movement speed for the right flipper
