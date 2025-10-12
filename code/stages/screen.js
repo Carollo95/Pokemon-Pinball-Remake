163ff6a4a693d0f05f1290d8f30a3d524daff562
Pedro <carollo95@gmail.com>
2025-10-12 19:48:06 +0200

    Flip tile on capture screen

--- code/stages/screen.js
+++ code/stages/screen.js
@@ -49,6 +49,8 @@ class Screen {
         this.hideSprite.addAnimation('001-bw', Asset.getAnimation('001-bw'));
         this.hideSprite.layer = SCENARIO_LAYER + 1;
         this.hideSprite.visible = false;
+        this.hideSprite.draw = this.hideSpriteDraw;
+        this.hideSprite.hideLevel = 0;
 
         this.spinBW();
     }
@@ -101,24 +103,47 @@ class Screen {
 
     flipCapture() {
         if (this.state !== SCREEN_STATE.CAPTURE) return;
-        const spr = this.hideSprite;
 
-        this.hideSprite.draw = function () {
-            push();
+        this.hideSprite.hideLevel++;
+    }
 
+    hideSpriteDraw() {
+        push();
 
-            
-            drawingContext.save();
-            drawingContext.beginPath();
-            drawingContext.rect(-this.width / 2, -this.height / 2, this.width / 2, this.height);
-            drawingContext.clip();
+        drawingContext.save();
+        drawingContext.beginPath();
 
-            this.ani.draw(0, 0);
-
-            drawingContext.restore();
-
-            pop();
+        if (this.hideLevel < 6) {
+            drawingContext.rect(this.width / 6, 0, this.width / 3, this.height / 2);
         }
+
+        if (this.hideLevel < 5) {
+            drawingContext.rect(-this.width / 6, -this.height / 2, this.width / 3, this.height / 2);
+        }
+
+        if (this.hideLevel < 4) {
+            drawingContext.rect(-this.width / 2, 0, this.width / 3, this.height / 2);
+        }
+
+        if (this.hideLevel < 3) {
+            drawingContext.rect(this.width / 6, -this.height / 2, this.width / 3, this.height / 2);
+        }
+
+        if (this.hideLevel < 2) {
+            drawingContext.rect(-this.width / 6, 0, this.width / 3, this.height / 2);
+        }
+
+        if (this.hideLevel < 1) {
+            drawingContext.rect(-this.width / 2, -this.height / 2, this.width / 3, this.height / 2);
+        }
+
+        drawingContext.clip();
+
+        this.ani.draw(0, 0);
+
+        drawingContext.restore();
+
+        pop();
     }
 
 }
