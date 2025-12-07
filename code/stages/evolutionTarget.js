a01cdb46fa45bbe184d20bfe4f9762be1443ca05
Pedro <carollo95@gmail.com>
2025-12-07 18:09:19 +0100

    Jumping experience

--- code/stages/evolutionTarget.js
+++ code/stages/evolutionTarget.js
@@ -1,6 +1,9 @@
-class EvolutionTarget{
+const EVOLUTION_TARGET_TIME_JUMP_INTERVAL = 750;
+const EVOLUTION_TARGET_TIME_JUMP_HIGH_POSITION_TIME = 100;
+const EVOLUTION_TARGET_HEIGHT_JUMP = 4;
 
-    constructor(x,y) {
+class EvolutionTarget {
+    constructor(x, y) {
         this.sprite = new Sprite(x, y, 16, 16, "none");
         this.sprite.debug = DEBUG;
         this.sprite.layer = SCENARIO_LAYER;
@@ -8,47 +11,58 @@ class EvolutionTarget{
         this.sprite.addAni('evolutionMethods', Asset.getAnimation('evolutionMethods'));
         this.sprite.ani.frame = 0;
         this.sprite.ani.playing = false;
+        this.baseY = y;
+
+        this.baseY = y;
+        this.lastJumpStart = 0;
+        this.jumping = false;
+        this.active = false;
+        this.callback = () => { };
     }
 
-    update(ballSprite){
-        if(this.active && this.sprite.overlaps(ballSprite)){
-            this.callback();
+    update(ballSprite) {
+        if (!this.active) return;
+
+        if (this.sprite.overlaps(ballSprite)) {
+            this.callback && this.callback();
             this.setActive(false);
+            return;
+        }
+
+        const now = millis();
+
+        if (!this.jumping && (now - this.lastJumpStart) >= EVOLUTION_TARGET_TIME_JUMP_INTERVAL) {
+            this.jumping = true;
+            this.lastJumpStart = now;
+            this.sprite.pos.y = this.baseY - EVOLUTION_TARGET_HEIGHT_JUMP;
+        }
+
+        if (this.jumping && (now - this.lastJumpStart) >= EVOLUTION_TARGET_TIME_JUMP_HIGH_POSITION_TIME) {
+            this.sprite.pos.y = this.baseY;
+            this.jumping = false;
         }
     }
 
-    setActive(active, callback){
-        this.active = active
-        this.sprite.visible = active;
-        this.callback = callback;
+    setActive(active, callback) {
+        this.active = !!active;
+        this.sprite.visible = !!active;
+        this.callback = callback || (() => { });
+
+        this.lastJumpStart = millis();
+        this.jumping = false;
+        if (!active) this.sprite.pos.y = this.baseY;
     }
 
     setEvolutionMethod(evolutionMethod) {
-        switch(evolutionMethod) {
-            case EVOLUTION_METHODS.EXPERIENCE:
-                this.sprite.ani.frame = 0;
-                break;
-            case EVOLUTION_METHODS.THUNDER_STONE:
-                this.sprite.ani.frame = 1;
-                break;
-            case EVOLUTION_METHODS.MOON_STONE:
-                this.sprite.ani.frame = 2;
-                break;
-            case EVOLUTION_METHODS.FIRE_STONE:
-                this.sprite.ani.frame = 3;
-                break;
-            case EVOLUTION_METHODS.LEAF_STONE:
-                this.sprite.ani.frame = 4;
-                break;
-            case EVOLUTION_METHODS.WATER_STONE:
-                this.sprite.ani.frame = 5;
-                break;
-            case EVOLUTION_METHODS.LINK_CABLE:
-                this.sprite.ani.frame = 6;
-                break;
-            default:
-                this.sprite.ani.frame = 0;
-                break;
+        switch (evolutionMethod) {
+            case EVOLUTION_METHODS.EXPERIENCE: this.sprite.ani.frame = 0; break;
+            case EVOLUTION_METHODS.THUNDER_STONE: this.sprite.ani.frame = 1; break;
+            case EVOLUTION_METHODS.MOON_STONE: this.sprite.ani.frame = 2; break;
+            case EVOLUTION_METHODS.FIRE_STONE: this.sprite.ani.frame = 3; break;
+            case EVOLUTION_METHODS.LEAF_STONE: this.sprite.ani.frame = 4; break;
+            case EVOLUTION_METHODS.WATER_STONE: this.sprite.ani.frame = 5; break;
+            case EVOLUTION_METHODS.LINK_CABLE: this.sprite.ani.frame = 6; break;
+            default: this.sprite.ani.frame = 0; break;
         }
     }
 }
