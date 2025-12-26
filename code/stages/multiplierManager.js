33e85434b8e68d143c5aac1032b03bf2958b32af
Pedro <carollo95@gmail.com>
2025-12-26 13:27:44 +0100

    more beheviour for multiplier

--- code/stages/multiplierManager.js
+++ code/stages/multiplierManager.js
@@ -1,11 +1,28 @@
+const MULTIPLIER_STATE = {
+    LEFT_BLINK: 'leftBlink',
+    RIGHT_BLINK: 'rightBlink',
+    BOTH_BLINK: 'bothBlink',
+}
+
 class MultiplierManager {
 
     constructor(onLeftMultiplierHitCallback, onRightMultiplierHitCallback) {
+        this.onLeftMultiplierHitCallback = onLeftMultiplierHitCallback;
+        this.onRightMultiplierHitCallback = onRightMultiplierHitCallback;
+
         this.leftMultiplier = new MultiplierTarget(85, 298, 75, 281, this.leftMultiplierHitCallback);
         this.rightMultiplier = new MultiplierTarget(233, 298, 245, 281, this.rightMultiplierHitCallback);
 
-        this.onLeftMultiplierHitCallback = onLeftMultiplierHitCallback;
-        this.onRightMultiplierHitCallback = onRightMultiplierHitCallback;
+        this.setInitialState();
+        this.status = MULTIPLIER_STATE.LEFT_BLINK;
+    }
+
+    setInitialState() {
+        this.multiplier = 1;
+        this.leftMultiplier.blink();
+        this.leftMultiplier.setNumber(0);
+        this.rightMultiplier.turnOff();
+        this.rightMultiplier.setNumber(2);
     }
 
     update(ballSprite) {
